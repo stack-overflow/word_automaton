648c818c2428f7309a15f1a030ee079f7c41e191
Tomek <uberthrash@gmail.com>
2015-01-25 04:50:19 +0100

    Triple

--- WordAutomationSharp/WordAutomationSharp/MainWindow.xaml.cs
+++ WordAutomationSharp/WordAutomationSharp/MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace WordAutomationSharp
 {
@@ -32,7 +35,8 @@ namespace WordAutomationSharp
         }
 
         WordGraph graph;
-        Dictionary<List<string>, Dictionary<string, int>> wordListMap = new Dictionary<List<string>, Dictionary<string, int>>(new ListComparer<string>());
+        Dictionary<Tuple<string, string>, Dictionary<string, int>> wordListMap = new Dictionary<Tuple<string, string>, Dictionary<string, int>>();
+        Dictionary<Tuple<string, string, string>, Dictionary<string, int>> tripleWords = new Dictionary<Tuple<string, string, string>, Dictionary<string, int>>();
         void CreateTwoPrevsMap(string[] words)
         {
             string pp = null;
@@ -54,37 +58,101 @@ namespace WordAutomationSharp
                     pp = words[i - 2].ToLower();
                     p = words[i - 1].ToLower();
                 }
-                var l = new List<string>() { pp, p };
-                if (!wordListMap.ContainsKey(l))
+                var tuple = Tuple.Create(pp, p);
+                if (!wordListMap.ContainsKey(tuple))
                 {
-                    wordListMap[l] = new Dictionary<string, int>();
+                    wordListMap[tuple] = new Dictionary<string, int>();
                 }
-                if (!wordListMap[l].ContainsKey(current))
+                if (!wordListMap[tuple].ContainsKey(current))
                 {
-                    wordListMap[l].Add(current, 0);
+                    wordListMap[tuple].Add(current, 0);
                 }
-                ++wordListMap[l][current];
+                ++wordListMap[tuple][current];
+            }
+        }
+        void CreateTriplePrevsMap(string[] words)
+        {
+            string ppp = null;
+            string pp = null;
+            string p = null;
+            for (int i = 0; i < words.Length; ++i)
+            {
+                string current = words[i].ToLower();
+                if (i == 0)
+                {
+                    pp = p = null;
+                }
+                else if (i == 1)
+                {
+                    pp = null;
+                    p = words[i - 1].ToLower();
+                }
+                else if (i == 2)
+                {
+                    ppp = null;
+                    pp = words[i - 2].ToLower();
+                    p = words[i - 1].ToLower();
+                }
+                else
+                {
+                    ppp = words[i - 3].ToLower();
+                    pp = words[i - 2].ToLower();
+                    p = words[i - 1].ToLower();
+                }
+                var triple = Tuple.Create(ppp, pp, p);
+                if (!tripleWords.ContainsKey(triple))
+                {
+                    tripleWords[triple] = new Dictionary<string, int>();
+                }
+                if (!tripleWords[triple].ContainsKey(current))
+                {
+                    tripleWords[triple].Add(current, 0);
+                }
+                ++tripleWords[triple][current];
             }
         }
         IEnumerable<string> GetCandidates(string[] words, int numCandidates)
         {
-            List<string> lastTwoWords = words.Select(s => s).Where(s => s != "").Skip(Math.Max(0, words.Count() - 2)).Take(2).ToList();
-            if (words.Length == 0)
+            var lastThreeWords = words.Select(s => s).Where(s => s != "").Skip(Math.Max(0, words.Count() - 3)).ToArray();
+            Tuple<string, string> tuple;
+            Tuple<string, string, string> triple;
+
+            if (lastThreeWords.Length == 0)
             {
-                lastTwoWords.Add(null); lastTwoWords.Add(null);
+                tuple = Tuple.Create<string, string>(null, null);
+                triple = Tuple.Create<string, string, string>(null, null, null);
             }
-            else if (words.Length == 1)
+            else if (lastThreeWords.Length == 1)
             {
-                lastTwoWords = new List<string>() { null, lastTwoWords.First() };
+                tuple = Tuple.Create<string, string>(null, lastThreeWords.First());
+                triple = Tuple.Create<string, string, string>(null, null, lastThreeWords.First());
             }
-            IEnumerable<string> sortedRights;
-            if (wordListMap.ContainsKey(lastTwoWords))
+            else if (lastThreeWords.Length == 2)
             {
-                sortedRights = (from entry in wordListMap[lastTwoWords].Keys orderby wordListMap[lastTwoWords][entry] descending select entry).Take(numCandidates);
+                tuple = Tuple.Create<string, string>(lastThreeWords.First(), lastThreeWords.Last());
+                triple = Tuple.Create<string, string, string>(null, lastThreeWords.First(), lastThreeWords.Last());
             }
             else
             {
-                sortedRights = null;
+                tuple = Tuple.Create<string, string>(lastThreeWords[1], lastThreeWords[2]);
+                triple = Tuple.Create<string, string, string>(lastThreeWords[0], lastThreeWords[1], lastThreeWords[2]);
+            }
+
+            IEnumerable<string> sortedRights;
+            if (tripleWords.ContainsKey(triple))
+            {
+                sortedRights = (from entry in tripleWords[triple].Keys orderby tripleWords[triple][entry] descending select entry).Take(numCandidates);
+                MessageBox.Show("A Triple Win");
+            }
+            else if (wordListMap.ContainsKey(tuple))
+            {
+                sortedRights = (from entry in wordListMap[tuple].Keys orderby wordListMap[tuple][entry] descending select entry).Take(numCandidates);
+                MessageBox.Show("A Double Win");
+            }
+            else
+            {
+                var word = words.Last(s => s != "");
+                sortedRights = (from entry in graph.WordNodes[word].Rights orderby entry.Value descending select entry.Key.NormalizedName).Take(numCandidates);
             }
             return sortedRights;
         }
@@ -99,29 +167,33 @@ namespace WordAutomationSharp
         }
 
         private void ReadFile(string file){
-            var t = new Thread(() =>{
-                var cfr = new SentenceFileReader(file);
-                this.graph = new WordGraph();
-                var str = "";
-                while (cfr.HasMore()){
-                    progress.Dispatcher.Invoke(() => progress.Value = cfr.GetProgress());
-                    var sentence = cfr.GetNextSentence();
-                    var words = sentence.ExtractWords();
-                    if (words.Length > 1){
-                        for (int i = 0, j = 1; j < words.Length; ++i, ++j){
-                            graph.MakeLink(words[i], words[j]);
+            string connectionString = Properties.Settings.Default.words_dbConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                var t = new Thread(() =>{
+                    var cfr = new SentenceFileReader(file);
+                    this.graph = new WordGraph();
+                    var str = "";
+                    while (cfr.HasMore()){
+                        progress.Dispatcher.Invoke(() => progress.Value = cfr.GetProgress());
+                        var sentence = cfr.GetNextSentence();
+                        var words = sentence.ExtractWords();
+                        if (words.Length > 1){
+                            for (int i = 0, j = 1; j < words.Length; ++i, ++j){
+                                graph.MakeLink(words[i], words[j]);
+                            }
+                        } else if (words.Length == 1 && words[0] != ""){
+                            graph.GetOrCreate(words[0]);
                         }
-                    } else if (words.Length == 1 && words[0] != ""){
-                        graph.GetOrCreate(words[0]);
+                        CreateTwoPrevsMap(words);
+                        CreateTriplePrevsMap(words);
                     }
-                    CreateTwoPrevsMap(words);
-                }
-                Dispatcher.Invoke(() => progress.Value = 100.0);
-                Dispatcher.Invoke(ShowTree);
-            });
-            t.IsBackground = false;
-            t.Start();
-            
+                    Dispatcher.Invoke(() => progress.Value = 100.0);
+                    Dispatcher.Invoke(ShowTree);
+                });
+                t.IsBackground = false;
+                t.Start();
+            }
         }
 
         private void OutputTreeView_Loaded(object sender, RoutedEventArgs e){
@@ -136,7 +208,7 @@ namespace WordAutomationSharp
             OutputTreeView.Items.Clear();
             treenodes.Clear();
             var t = new Thread(() =>{
-                var nodes = this.graph.WordNodes.Where(w=>(w.Value.TotalConnections>2*Threshold)).OrderBy(w => w.Key).ToList();
+                var nodes = this.graph.WordNodes.OrderBy(w => w.Key).ToList();
                 for(var i=0; i<nodes.Count; ++i){
                     Dispatcher.Invoke(() => progress.Value = (double)i/nodes.Count * 100.0);
                     var kv = nodes[i];
@@ -185,11 +257,11 @@ namespace WordAutomationSharp
             {
                 int numCandidates = 5;
                 var sortedRights = GetCandidates(words, numCandidates);
-                if (sortedRights == null)
-                {
-                    var word = words.Last(s => s != "");
-                    sortedRights = (from entry in graph.WordNodes[word].Rights orderby entry.Value descending select entry.Key.NormalizedName).Take(numCandidates);
-                }
+                //if (sortedRights == null)
+                //{
+                //    var word = words.Last(s => s != "");
+                //    sortedRights = (from entry in graph.WordNodes[word].Rights orderby entry.Value descending select entry.Key.NormalizedName).Take(numCandidates);
+                //}
 
                 listBox1.Items.Clear();
                 foreach (var zord in sortedRights)
